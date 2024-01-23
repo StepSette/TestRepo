8032166d20cd30115e64fa0f95d2c4577a82644d
StepSette <142811701+StepSette@users.noreply.github.com>
2024-01-23 17:03:51 -0300

    Create MyFirstPythononGitHub.py
    functional calculus of three variables

---
+++ MyFirstPythononGitHub.py
@@ -0,0 +1,12 @@
+# this is a basic calculation with three variables
+
+x = 10
+y = x * 45
+z = y * 100 - 3
+
+def calculus(x, y, z):
+  result = x + y + z
+  return(result)
+
+result = calculus(10, y, z) 
+print(result)
